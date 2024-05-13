c0a0fcff4cb07c26f6b7c71150d80eafc741b56a
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-05-13 11:03:36 +0100

    feat: added template prompt data

---
+++ src/data.ts
@@ -0,0 +1,24 @@
+import { assets } from "./assets/index";
+
+export const static_data = [
+  {
+    prompt:
+      " Help me plan a game night with 5 friends. I have dice and cards, but no board game would be willing to get board games for under $100",
+    icon: assets.compass_icon,
+  },
+  {
+    prompt:
+      "What are some tips to improve public speaking skills for beginners?",
+    icon: assets.bulb_icon,
+  },
+  {
+    prompt:
+      "Create a recipe for a low-carb meal with the following ingredients I have in my fridge: cauliflower, cucumber, and yogurt",
+    icon: assets.message_icon,
+  },
+  {
+    prompt:
+      "Help me write an email to Solar Toothbrush Co. requesting a refund for a product that came damaged. ",
+    icon: assets.code_icon,
+  },
+];
