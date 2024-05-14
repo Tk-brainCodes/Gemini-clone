cfb0837afa493f049099c74d202cab6df497ac79
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-05-14 09:32:18 +0100

    feat: sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,57 +1,83 @@
 import { useState } from "react";
 import { assets } from "../../assets/index.js";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils.js";
 
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
 
   return (
-    <div className='min-h-100 flex flex-col justify-between bg-[#f0f4f9] px-[25px] py-[15px]'>
-      <div className='block ml-[10px] cursor-pointer'>
+    <div
+      className={cn(
+        "min-h-100 flex flex-col items-start px-2 py-2 justify-between bg-[#f0f4f9] transition ease-in-out",
+        !extended ? "w-[100px] flex items-center justify-between" : ""
+      )}
+    >
+      <div className='block ml-[1px] mt-4 cursor-pointer'>
         <img
           onClick={() => setExtended((prev) => !prev)}
-          className='w-[20px]'
+          className='w-[20px] mb-[4em] ml-2'
           src={assets.menu_icon}
           alt='menu-icon'
         />
-        <div className='new-chat mt-[10px] flex items-center gap-[10px] px-[10px] py-[15px] bg-[#e6eaf1] rounded-[50px] tetx-[14px] text-gray-400 cursor-pointer'>
-          <img className='w-[20px]' src={assets.plus_icon} alt='plus-icon' />
-          {extended && <p>New Chat</p>}
-        </div>
+
+        <Button
+          className={cn(
+            "flex items-center justify-between bg-gray-200 hover:bg-[#dadfe8] mt-[20px] gap-[10px] px-[10px] py-[15px] rounded-[50px] text-[14px] cursor-pointer",
+            extended && "rounded-full"
+          )}
+        >
+          <span className='font-normal text-[32px] text-[#1f1f1f] mb-[8px]'>
+            +
+          </span>
+          {extended && (
+            <p className='text-[#1f1f1f] font-semibold text-[.875rem]'>
+              New Chat
+            </p>
+          )}
+        </Button>
         {extended && (
-          <div className='recent flex flex-col '>
-            <p className='mt-[30px] mb-[20px] '>Recent</p>
-            <div className='recent-entry flex text-start gap-[10px]  px-[10px] py-[10px] pr-[40px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#e2e6eb] transition-all ease-in-out'>
+          <div className='recent flex flex-col'>
+            <p className='mt-[30px] mb-[20px] ml-[13px] text-black font-normal text-[15px]'>
+              Recent
+            </p>
+            <div className='flex h-[34px] items-center justify-between gap-x-4 -mt-[15px] text-start gap-[10px] pr-[40px] rounded-[50px] text-black cursor-pointer leading-[1.5rem] font-medium text-[14px] hover:bg-[#e2e6eb] transition-all ease-in-out px-2 py-2 '>
               <img
                 className='w-[20px]'
                 src={assets.message_icon}
                 alt='message-cion'
               />
-              <p>What is react...</p>
+              <p>What is react and next</p>
+              <img
+                className='w-[20px]'
+                src={assets.message_icon}
+                alt='message-cion'
+              />
             </div>
           </div>
         )}
       </div>
 
-      <div className='pr-[10px] flex flex-col'>
-        <div className='flex text-start gap-[10px] px-[10px] py-[10px] pr-[40px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#e2e6eb] transition-all ease-in-out'>
+      <div className={cn(" flex flex-col", extended === true && "w-[100%]")}>
+        <div className='flex text-start w-[100%] items-center gap-[15px] h-[34px] rounded-[50px] text-black cursor-pointer leading-[1.5rem] font-medium text-[14px] hover:bg-[#e2e6eb] transition-all ease-in-out px-2 py-2'>
           <img
-            className='w-[20px]'
+            className='w-[20px] h-[20px] mt-1'
             src={assets.question_icon}
             alt='question-icon'
           />
           {extended && <p>Help</p>}
         </div>
-        <div className='flex text-start gap-[10px] px-[10px] py-[10px] pr-[40px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#e2e6eb] transition-all ease-in-out'>
+        <div className='flex  text-start h-[34px] items-center gap-[15px] rounded-[50px] text-black cursor-pointer leading-[1.5rem] font-medium text-[14px] hover:bg-[#e2e6eb] transition-all ease-in-out px-2 py-2'>
           <img
-            className='w-[20px]'
+            className='w-[20px] h-[20px] mt-1'
             src={assets.history_icon}
             alt='question-icon'
           />
           {extended && <p>Activity</p>}
         </div>
-        <div className='flex text-start gap-[10px]  px-[10px] py-[10px] pr-[40px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#e2e6eb] transition-all ease-in-out'>
+        <div className='flex text-start h-[34px]items-center gap-[15px] rounded-[50px] text-black cursor-pointer leading-[1.5rem] font-medium text-[14px] hover:bg-[#e2e6eb] transition-all ease-in-out px-2 py-2 '>
           <img
-            className='w-[20px]'
+            className='w-[20px] h-[20px] mt-1'
             src={assets.setting_icon}
             alt='question-icon'
           />
