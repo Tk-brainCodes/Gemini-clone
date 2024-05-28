2c9181999d9928fdffb3c191cc137d0dc07ae2f1
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-05-28 13:29:30 +0100

    feat: added popover trigger for settings button

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,28 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { assets } from "../../assets/index.js";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils.js";
+import { Context } from "@/context/global-context.js";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
+  const context = useContext(Context);
+
+  if (!context) {
+    return <div>Context not found.</div>;
+  }
+
+  const { onSent, prevPrompts, setRecentPrompt, newChatScreen } = context;
+
+  const loadPromptToInput = async (prompt: string) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
 
   return (
     <div
@@ -26,6 +44,7 @@ const Sidebar = () => {
             "flex items-center justify-between bg-gray-200 hover:bg-[#dadfe8] mt-[20px] gap-[10px] px-[10px] py-[15px] rounded-[50px] text-[14px] cursor-pointer",
             extended && "rounded-full"
           )}
+          onClick={newChatScreen}
         >
           <span className='font-normal text-[32px] text-[#1f1f1f] mb-[8px]'>
             +
@@ -41,19 +60,27 @@ const Sidebar = () => {
             <p className='mt-[30px] mb-[20px] ml-[13px] text-black font-normal text-[15px]'>
               Recent
             </p>
-            <div className='flex h-[34px] items-center justify-between gap-x-4 -mt-[15px] text-start gap-[10px] pr-[40px] rounded-[50px] text-black cursor-pointer leading-[1.5rem] font-medium text-[14px] hover:bg-[#e2e6eb] transition-all ease-in-out px-2 py-2 '>
-              <img
-                className='w-[20px]'
-                src={assets.message_icon}
-                alt='message-cion'
-              />
-              <p>What is react and next</p>
-              <img
-                className='w-[20px]'
-                src={assets.message_icon}
-                alt='message-cion'
-              />
-            </div>
+            {prevPrompts.map((text: string, index: number) => {
+              return (
+                <div
+                  key={index}
+                  onClick={() => loadPromptToInput(text)}
+                  className='flex h-[34px] items-center justify-between  mt-[5px] text-start gap-[10px] pr-[40px] rounded-[50px] text-black cursor-pointer leading-[1.5rem] font-medium text-[14px] hover:bg-[#e2e6eb] transition-all ease-in-out px-2 py-2 '
+                >
+                  <img
+                    className='w-[20px]'
+                    src={assets.message_icon}
+                    alt='message-cion'
+                  />
+                  <p>{text.slice(0, 18)}...</p>
+                  <img
+                    className='w-[20px]'
+                    src={assets.message_icon}
+                    alt='message-cion'
+                  />
+                </div>
+              );
+            })}
           </div>
         )}
       </div>
@@ -75,14 +102,19 @@ const Sidebar = () => {
           />
           {extended && <p>Activity</p>}
         </div>
-        <div className='flex text-start h-[34px]items-center gap-[15px] rounded-[50px] text-black cursor-pointer leading-[1.5rem] font-medium text-[14px] hover:bg-[#e2e6eb] transition-all ease-in-out px-2 py-2 '>
-          <img
-            className='w-[20px] h-[20px] mt-1'
-            src={assets.setting_icon}
-            alt='question-icon'
-          />
-          {extended && <p>Setting</p>}
-        </div>
+        <Popover>
+          <div className='flex text-start h-[34px]items-center gap-[15px] rounded-[50px] text-black cursor-pointer leading-[1.5rem] font-medium text-[14px] hover:bg-[#e2e6eb] transition-all ease-in-out px-2 py-2 '>
+            <PopoverTrigger>
+              <img
+                className='w-[20px] h-[20px] mt-1'
+                src={assets.setting_icon}
+                alt='question-icon'
+              />
+              {extended && <p>Setting</p>}
+            </PopoverTrigger>
+            <PopoverContent>Place content for the popover here.</PopoverContent>
+          </div>
+        </Popover>
       </div>
     </div>
   );
