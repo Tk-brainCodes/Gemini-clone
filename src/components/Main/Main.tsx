3bb17dacd30aa285abd901e40f9b36efe1cd816e
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-05-28 13:28:46 +0100

    feat: re-ordered context import

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,157 +1,197 @@
-import { ChangeEvent, useContext } from "react";
-
+import { ChangeEvent, useContext, useState } from "react";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-
 import { Button } from "@/components/ui/button";
 import { assets } from "@/assets/index";
-import { Input } from "@/components/ui/input";
-
 import { static_data } from "../../data";
 import { Context } from "@/context/global-context";
+import { AnimatePresence, motion } from "framer-motion";
 import DOMPurify from "dompurify";
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
 
 const Main = () => {
+  const context = useContext(Context);
+  const [cardPrompt, setCardPrompt] = useState("");
+
+  if (!context) {
+    return <div>Error: Context not found</div>;
+  }
+
   const {
-    prevPrompts,
-    setPrevPrompts,
     onSent,
-    setRecentPrompt,
     recentPrompt,
     showResult,
     loading,
     resultData,
     input,
     setInput,
-  } = useContext(Context);
+    setRecentPrompt,
+  } = context;
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const cursorPosition = e.currentTarget.selectionStart || 0;
+      const newValue =
+        input.slice(0, cursorPosition) + "\n" + input.slice(cursorPosition);
+
+      setInput(newValue);
+    }
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const setCardPromptToInput = (prompt: string) => {
+    setCardPrompt(prompt);
+    setRecentPrompt(prompt);
+  };
 
   return (
-    <div className='flex-1 min-h-[100vh] pb-[15vh] relative'>
-      <div className='flex items-center justify-between px-4 py-2'>
-        <DropdownMenu>
-          <DropdownMenuTrigger>Gemini</DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem>Gemini</DropdownMenuItem>
-            <DropdownMenuItem>Gemini Advanced</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+    <AnimatePresence>
+      <div className='flex-1 min-h-[100vh] pb-[15vh] relative'>
+        <div className='flex items-center justify-between px-4 py-2'>
+          <DropdownMenu>
+            <DropdownMenuTrigger>Gemini</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>Gemini</DropdownMenuItem>
+              <DropdownMenuItem>Gemini Advanced</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
 
-        <div className='flex gap-x-6'>
-          <Button variant='outline'>Try Gemini Advanced</Button>
-          <img
-            src={assets.user_icon}
-            alt='user-profile-icon'
-            className='w-[40px] h-[40px] rounded-full'
-          />
-        </div>
-      </div>
-
-      <div className='w-full max-w-[950px] m-auto h-auto flex flex-col items-start justify-center'>
-        {!showResult ? (
-          <>
-            <span className='text-[56px] leading-[3.5rem] text-[#c4c7c5] font-medium p-[20px] mt-5 mb-[40px]'>
-              <p className='gradient-text'>Hello, Eboreime</p>
-              <p>How can i help you today?</p>
-            </span>
-            <div className='grid-card-template'>
-              {static_data.map(
-                ({ prompt, icon }: { prompt: string; icon: string }) => (
-                  <div
-                    key={prompt}
-                    className='h-[200px] p-[15px] bg-[#f0f4f9] rounded-[13px] relative cursor-pointer hover:bg-[#dfe4ed]'
-                  >
-                    <p className='text-black text-[17px] flex flex-col items-start '>
-                      {prompt}
-                    </p>
-                    <span>
-                      <img
-                        src={icon}
-                        alt=''
-                        className='w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]'
-                      />
-                    </span>
-                  </div>
-                )
-              )}
-            </div>
-          </>
-        ) : (
-          <div className='result'>
-            <div className='my-10 flex items-center gap-[20px]'>
-              <img
-                src={assets.user_icon}
-                alt='user-icon'
-                className='w-[40px] h-[40px] rounded-full'
-              />
-              <p>{recentPrompt}</p>
-            </div>
-            <div className='flex items-start gap-[20px]'>
-              <img
-                src={assets.gemini_icon}
-                alt='gemini-icon'
-                className='w-[40px] h-[40px]'
-              />
-              {loading ? (
-                <div className='w-[700px] flex flex-col gap-[10px]'>
-                  <hr className='skeleton-loader' />
-                  <hr className='skeleton-loader' />
-                  <hr className='skeleton-loader' />
-                </div>
-              ) : (
-                <p
-                  className='text-[17px] font-normal text-base leading-[1.7] text-[#1f1f1f]'
-                  dangerouslySetInnerHTML={{
-                    __html: DOMPurify.sanitize(resultData),
-                  }}
-                ></p>
-              )}
-            </div>
-          </div>
-        )}
-
-        <div className='search-container  bg-white h-[100px] w-full bg-gradient-to-b from-transparent via-white to-white'>
-          <div className='search-box mt-4'>
-            <Input
-              type='text'
-              placeholder='Enter a text here'
-              className='flex-1 bg-transparent border-0 outline-0 focus:outline-0 p-[8px] text-[18px]'
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                setInput(e.target.value)
-              }
-              value={input}
+          <div className='flex gap-x-6'>
+            <Button variant='outline'>Try Gemini Advanced</Button>
+            <img
+              src={assets.user_icon}
+              alt='user-profile-icon'
+              className='w-[40px] h-[40px] rounded-full'
             />
-            <div className='flex gap-x-4'>
-              <img
-                src={assets.gallery_icon}
-                alt='gallery-icon'
-                className='w-[24px]'
-              />
-              <img src={assets.mic_icon} alt='mic-icon' className='w-[24px]' />
-              <img
-                src={assets.send_icon}
-                alt='send icon'
-                className='w-[24px]'
-                onClick={() => onSent()}
-              />
-            </div>
           </div>
+        </div>
 
-          <p className='mx-auto my-15px text-[13px] text-center text-black font-normal mb-1'>
-            Gemini may display inaccurate info, including about people, so
-            double-check its responses.{" "}
-            <span className='underline cursor-pointer'>
-              Your privacy & Gemini Apps
-            </span>
-          </p>
+        <div className='w-full max-w-[950px] m-auto h-auto flex flex-col items-start justify-center'>
+          {!showResult ? (
+            <>
+              <span className='text-[56px] leading-[3.5rem] text-[#c4c7c5] font-medium p-[20px] mt-5 mb-[40px]'>
+                <p className='gradient-text'>Hello, Eboreime</p>
+                <p>How can I help you today?</p>
+              </span>
+              <div className='grid-card-template'>
+                {static_data.map(
+                  ({ prompt, icon }: { prompt: string; icon: string }) => (
+                    <div
+                      key={prompt}
+                      className='h-[200px] p-[15px] bg-[#f0f4f9] rounded-[13px] relative cursor-pointer hover:bg-[#dfe4ed]'
+                      onClick={() => setCardPromptToInput(prompt)}
+                    >
+                      <p className='text-black text-[17px] flex flex-col items-start '>
+                        {prompt}
+                      </p>
+                      <span>
+                        <img
+                          src={icon}
+                          alt=''
+                          className='w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]'
+                        />
+                      </span>
+                    </div>
+                  )
+                )}
+              </div>
+            </>
+          ) : (
+            <div className='result'>
+              <div className='my-10 flex items-center gap-[20px]'>
+                <img
+                  src={assets.user_icon}
+                  alt='user-icon'
+                  className='w-[40px] h-[40px] rounded-full'
+                />
+                <p>{recentPrompt}</p>
+              </div>
+              <div className='flex items-start gap-[20px]'>
+                <img
+                  src={assets.gemini_icon}
+                  alt='gemini-icon'
+                  className='w-[40px] h-[40px]'
+                />
+                {loading ? (
+                  <div className='w-[700px] flex flex-col gap-[10px]'>
+                    <hr className='skeleton-loader' />
+                    <hr className='skeleton-loader' />
+                    <hr className='skeleton-loader w-[70%]' />
+                  </div>
+                ) : (
+                  <p
+                    className='text-[17px] font-normal text-base leading-[1.7] text-[#1f1f1f]'
+                    dangerouslySetInnerHTML={{
+                      __html: DOMPurify.sanitize(resultData),
+                    }}
+                  ></p>
+                )}
+              </div>
+            </div>
+          )}
+
+          <motion.div className='search-container bg-white h-[100px] w-full bg-gradient-to-b from-transparent via-white to-white'>
+            <motion.div className='search-box mt-4'>
+              <input
+                type='text'
+                placeholder='Enter a text here'
+                className='flex-1 bg-transparent border-1 border-blue-400 outline-none focus:outline-none focus:border-none focus:ring-0 p-[8px] text-[18px]'
+                value={input || cardPrompt}
+                onKeyDown={handleKeyDown}
+                style={{ whiteSpace: "pre-wrap" }}
+                onChange={handleInputChange}
+              />
+
+              <motion.div className='flex gap-x-4'>
+                <span className='rounded-full hover:bg-slate-200 px-2 py-2 flex items-center justify-center'>
+                  <img
+                    src={assets.gallery_icon}
+                    alt='gallery-icon'
+                    className='w-[24px] cursor-pointer'
+                  />
+                </span>
+
+                <span className='rounded-full hover:bg-slate-200 px-2 py-2 flex items-center justify-center'>
+                  <img
+                    src={assets.mic_icon}
+                    alt='mic-icon'
+                    className='w-[24px] cursor-pointer'
+                  />
+                </span>
+                {input ||
+                  (cardPrompt.length > 1 && (
+                    <motion.span
+                      className='rounded-full hover:bg-slate-200 px-2 py-2 flex items-center justify-center'
+                      onClick={() => onSent(input)}
+                    >
+                      <motion.img
+                        src={assets.send_icon}
+                        alt='send icon'
+                        className='w-[24px] cursor-pointer '
+                      />
+                    </motion.span>
+                  ))}
+              </motion.div>
+            </motion.div>
+
+            <p className='mx-auto my-15px text-[13px] text-center text-black font-normal mb-1'>
+              Gemini may display inaccurate info, including about people, so
+              double-check its responses.{" "}
+              <span className='underline cursor-pointer'>
+                Your privacy & Gemini Apps
+              </span>
+            </p>
+          </motion.div>
         </div>
       </div>
-    </div>
+    </AnimatePresence>
   );
 };
 
